a72c020abd8910db704fd386ff37d1664c0ab158
Vinícius Nascimento <vini_sn@outlook.com>
2024-02-01 08:17:09 -0300

    Create DesafioMod5.java

---
+++ exercicio/src/classe/DesafioMod5.java
@@ -0,0 +1,10 @@
+package classe;
+
+public class DesafioMod5 {
+    int a = 3;
+
+    public static void main(String[] args) {
+        DesafioMod5 desafio = new DesafioMod5();
+        System.out.println(desafio.a);
+    }
+}
