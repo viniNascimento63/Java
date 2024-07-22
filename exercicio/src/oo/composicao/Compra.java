ec180544bb366a9a7f44f7be5deb78b98a5e7d96
Vinícius Nascimento <vini_sn@outlook.com>
2024-07-22 14:28:41 -0300

    POO - Composição: Relacionamento Muitos para Muitos

---
+++ exercicio/src/oo/composicao/Compra.java
@@ -0,0 +1,28 @@
+package oo.composicao;
+
+import java.util.ArrayList;
+
+public class Compra {
+
+    String cliente;
+    ArrayList<Item> carrinho = new ArrayList<>();
+
+    void adicionarItem(Item item) {
+        this.carrinho.add(item);
+        item.compra = this;
+    }
+
+    void adicionarItem(String nome, int quantidade, double preco) {
+        this.adicionarItem(new Item(nome, quantidade, preco));
+    }
+
+    double getTotal() {
+        double total = 0;
+
+        for (Item item: carrinho) {
+            total += item.preco * item.quantidade;
+        }
+
+        return total;
+    }
+}
